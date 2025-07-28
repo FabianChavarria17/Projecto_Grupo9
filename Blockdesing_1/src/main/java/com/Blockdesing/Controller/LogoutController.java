2f05a4aa67b2ef78bbeb10d2be43c6221bf67d07
Roberto <145409693+Robert2496M@users.noreply.github.com>
2025-07-28 14:10:22 -0600

    Commit Autentificacion
    Cuando se hace log in desaparecen las opciones de registro y log in y se agrego el nombre del ususario, un drop down para ir al perfil y para cerrar sesion.

---
+++ Blockdesing_1/src/main/java/com/Blockdesing/Controller/LogoutController.java
@@ -0,0 +1,16 @@
+
+package com.Blockdesing.Controller;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LogoutController {
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/";  // Redirige al home
+    }
+}
