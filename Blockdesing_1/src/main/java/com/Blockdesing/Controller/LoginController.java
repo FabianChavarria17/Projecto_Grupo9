2e7289dbc7d3f8f3da4206e005ab954b6fbc8156
Roberto <145409693+Robert2496M@users.noreply.github.com>
2025-07-28 13:48:50 -0600

    Commit log In
    Commit inicio de sesion y redirige al index (home)

--- Blockdesing_1/src/main/java/com/Blockdesing/Controller/LoginController.java
+++ Blockdesing_1/src/main/java/com/Blockdesing/Controller/LoginController.java
@@ -7,6 +7,7 @@ import com.Blockdesing.Domain.Usuario;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,23 +20,48 @@ public class LoginController {
 
     @Autowired
     private UsuarioDao usuarioDao;
-
-    @PostMapping("/login")
-    public String login(@RequestParam String correo, @RequestParam String password,RedirectAttributes redirectAttributes,
+    
+    @GetMapping
+    public String showLoginPage() {
+        return "login";            // login.html en templates/
+    }
+    
+     @PostMapping
+    public String login(
+            @RequestParam String correo,
+            @RequestParam String password,
+            RedirectAttributes redirectAttributes,
             HttpSession session) {
+
         Usuario usuario = usuarioDao.findByCorreoAndPassword(correo, password);
         if (usuario != null && usuario.isActivo()) {
-            // Guardar usuario en sesión (opcional)
             session.setAttribute("usuarioLogueado", usuario);
-            // Redireccionar al inicio con mensaje
-            redirectAttributes.addFlashAttribute("mensaje", "Login exitoso. ¡Bienvenido " + usuario.getNombre() + "!");
-            return "redirect:/";
-            /*return "Login exitoso. Bienvenido, " + usuario.getNombre();*/
+            redirectAttributes.addFlashAttribute("mensaje",
+                "Login exitoso. ¡Bienvenido " + usuario.getNombre() + "!");
+            return "redirect:/";    // página home
         } else {
-            // Redireccionar a login con error
-            redirectAttributes.addFlashAttribute("error", "Correo o contraseña incorrectos.");
-            return "redirect:/login";
-            /*return "Credenciales inválidas o cuenta inactiva";*/
+            redirectAttributes.addFlashAttribute("error",
+                "Correo o contraseña incorrectos.");
+            return "redirect:/login"; // de nuevo al login
         }
     }
+
+//    @PostMapping("/login")
+//    public String login(@RequestParam String correo, @RequestParam String password,RedirectAttributes redirectAttributes,
+//            HttpSession session) {
+//        Usuario usuario = usuarioDao.findByCorreoAndPassword(correo, password);
+//        if (usuario != null && usuario.isActivo()) {
+//            // Guardar usuario en sesión (opcional)
+//            session.setAttribute("usuarioLogueado", usuario);
+//            // Redireccionar al inicio con mensaje
+//            redirectAttributes.addFlashAttribute("mensaje", "Login exitoso. ¡Bienvenido " + usuario.getNombre() + "!");
+//            return "redirect:/index";
+//            /*return "Login exitoso. Bienvenido, " + usuario.getNombre();*/
+//        } else {
+//            // Redireccionar a login con error
+//            redirectAttributes.addFlashAttribute("error", "Correo o contraseña incorrectos.");
+//            return "redirect:/login";
+//            /*return "Credenciales inválidas o cuenta inactiva";*/
+//        }
+//    }
 }
